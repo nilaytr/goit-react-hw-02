427427374d0ff3b3428e1ce3d9a3a0da098649b3
Nilay Türker <nilaytr89qgmail.com>
2025-03-01 21:02:42 +0300

    add react icon

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,7 +1,9 @@
+import { IoCafeOutline } from "react-icons/io5";
 const Description = () => {
     return (
         <div>
-            <h1>Sip Happens Café</h1>
+            <h1 className="coffee-icon" size="48">Sip Happens Café</h1>
+            <IoCafeOutline className="inline-block mr-2" size={48}/>
             <p>Please leave your feedback about our service by selecting one of the options below.</p>
         </div>
     );
